f81d7b0caaa24a7218a1145bb3ce5eaa290f7c0c
Victor Martinez <vimar@eg.dk>
2023-10-07 23:41:45 +0200

    Change default values

--- ComputerAI.Dialogue/Program.cs
+++ ComputerAI.Dialogue/Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    private const int MAX_INTERACTIONS = 5;
+    private const int MAX_INTERACTIONS = 2;
 
     /*
      * For a list of voices, check https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=tts
@@ -14,7 +14,7 @@ class Program
         var bot1 = new Bot("Anakin", "en-US-DavisNeural", "You are talking to Obi-wan Kenobi", "Let's roleplay, you will act as Anakin Skywalker");
         var bot2 = new Bot("Obi-wan", "en-US-SteffanNeural", "You are talking to Anakin Skywalker", "Let's roleplay, you will act as Obi-wan Kenobi");
 
-        string msgBot2 = "You were the chosen one";
+        string msgBot2 = "You were my brother";
         var msgBot1 = await bot1.Answer(msgBot2);
 
         int counter = 0;
